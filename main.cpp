2d72b0f5db3e0904b7fb7d5fc9ddc985a9324342
XiDianZuoYun <805701948@qq.com>
2018-05-04 15:11:01 +0800

    test

--- main.cpp
+++ main.cpp
@@ -1,1 +1,13 @@
-
+#include<iostream>
+#include "eventloop.hpp"
+#include "Channel.hpp"
+int main()
+{
+    EventLoop loop;
+    Channel ch(STDIN_FILENO,EPOLLET|EPOLLIN,20,&loop);
+    Channel* _ch=&ch;
+    loop.updateChannel(_ch);
+    loop.loop();
+    loop.quit();
+    return 0;
+}
