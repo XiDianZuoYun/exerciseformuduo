adda3378a506373df0a0a43bb19bf496a3ca6ca0
XiDianZuoYun <805701948@qq.com>
2018-10-16 19:54:15 -0700

    Use exception

---
+++ base/commonexception.cpp
@@ -0,0 +1,29 @@
+#include <string.h>
+#include <error.h>
+#include "commonexception.h"
+
+CommonException::CommonException(const std::string& msg_,ErrType type) throw():msg(msg_),etype(type)
+{
+    switch (type) {
+    case ErrType::SOCKETErr:
+        msg.append(" SOCKET ERROR:");
+        msg.append(strerror(errno));
+        break;
+    case ErrType::FILEEXCErr:
+        msg.append(" FILE ERROR:");
+        msg.append(strerror(errno));
+        break;
+    case ErrType::POLLERErr:
+        msg.append(" POLLER ERROR:");
+        msg.append(strerror(errno));
+        break;
+    case ErrType::TIMERErr:
+        msg.append(" TIMER ERROR:");
+        msg.append(strerror(errno));
+    case ErrType::BUFFERErr:
+        msg.append(" BUFFER ERROR:");
+        msg.append(strerror(errno));
+    default:
+        break;
+    }
+}
